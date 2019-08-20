14073dc018db5752a4d6bf500647f58e43606664
cypggs <qcypggs@qq.com>
2019-08-20 21:17:18 +0800

    sd

---
+++ src/main/java/com/example/demo/DemoJinkinsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJinkinsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoJinkinsServiceApplication.class, args);
+    }
+
+}
